76a32c68bf01ae6c19e503876cc96746d7f0eca0
Sebastianrosberg <sebbe.rosberg@gmail.com>
2024-11-18 15:23:19 +0100

    Funktion för att få distans.
    Jag har påbörjat en funktion där jag har loopat igenom distances för att få ut distansen i rutnätet. Jag har även påbörjat en prompt funktion för att jämföra namn på prompten och om de blir sant.

--- index.js
+++ index.js
@@ -14,13 +14,53 @@ function cityNames() {
 }
 
 function cityDistances() {
-    for (let distance of distances) {
-        console.log(distance)
-        let distanceContainer = document.getElementById("")
+    for (let distance1 of distances) {
+        let distanceContainer = document.getElementById("table")
+        let currentDistance = distance1.distance
+        let distanceCell = document.createElement("div")
+        distanceContainer.appendChild(distanceCell)
+        distanceCell.classList.add("cell")
+        distanceCell.textContent = currentDistance / 10
+        // console.log(currentDistance)
+
+        //fortsätt här
+
+    }
+}
+let userPrompt = prompt("Skriv en stad.")
+
+function cityDistance(userPrompt) {
+    for (let city of cities) {
+        if (userPrompt == city.name) {
+            let city_id = city.id;
+            console.log("DEN MATCHAR")
+            for (let distance1 of distances) {
+                let city_1 = distance1.city1;
+                let city_2 = distance1.city2;
+                if (city_id == city_1 || city_2 == city_id) {
+                    console.log(true)
+
+
+                }
+
+            }
+        }
+
+        /*
+            1. Jag har nu city_id:t
+            2. Jag måste gå igenom distance
+                2.1 Ta ut de objekten som innehåller något om den nuvarande staden
+                2.2 Göra något med det.......
+        */
+
 
     }
 }
 
+//om stadens namn går med id i distances så mät avstånd.
+
+
+cityDistance(userPrompt)
 cityNames()
 cityDistances()
 
